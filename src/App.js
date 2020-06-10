4ae05d65982060b85ea0003f777a9a92f53e667e
yuzzon <yuzzon@gmail.com>
2020-06-10 21:02:04 +0300

    GA call

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import ReactGA from 'react-ga';
 
-function initializeReactGA() {
+
    ReactGA.initialize('UA-169056750-1');
    ReactGA.pageview('/homepage');
-}
+
 
 function App() {
    return (
